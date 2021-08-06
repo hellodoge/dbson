780fca63fa0288a784f2071ded5762e747e681ec
waldemar <hellodoge@tuta.io>
2021-08-06 18:30:21 +0300

    add generic "reverse bytes" function

---
+++ include/util/bswap.hpp
@@ -0,0 +1,18 @@
+#ifndef DBSON_BSWAP_HPP
+#define DBSON_BSWAP_HPP
+
+namespace util {
+    template <typename T>
+    T bswap(T x) {
+        char *s = reinterpret_cast<char *>(&x);
+        char *e = s + sizeof(T) - 1;
+        while (s < e) {
+            char tmp = *s;
+            *s = *e;
+            *e = tmp;
+            *s++, *e--;
+        }
+    }
+}
+
+#endif //DBSON_BSWAP_HPP
